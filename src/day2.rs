4b4826ca855b834c024f09fe9d3835972e80766a
Spanfile <spansdev@gmail.com>
2019-12-02 18:51:30 +0200

    Okay now it works for real

--- src/day2.rs
+++ src/day2.rs
@@ -13,26 +13,53 @@ pub fn part1(input: &[i32]) -> i32 {
     input[1] = 12;
     input[2] = 2;
 
+    intcode(input)
+}
+
+#[aoc(day2, part2)]
+pub fn part2(input: &[i32]) -> i32 {
+    let input: Vec<i32> = input.iter().map(|i| *i).collect();
+
+    for verb in 0..99 {
+        for noun in 0..99 {
+            let mut memory = input.clone();
+            memory[1] = noun;
+            memory[2] = verb;
+
+            if intcode(memory) == 19690720 {
+                return 100 * noun + verb;
+            }
+        }
+    }
+
+    0
+}
+
+fn intcode(mut memory: Vec<i32>) -> i32 {
     let mut index = 0;
     loop {
-        match input[index] {
+        match memory[index] {
             1 => {
-                let dest = input[index + 3] as usize;
-                input[dest] = input[index + 1] + input[index + 2];
+                let i1 = memory[index + 1] as usize;
+                let i2 = memory[index + 2] as usize;
+                let dest = memory[index + 3] as usize;
+                memory[dest] = memory[i1] + memory[i2];
             }
             2 => {
-                let dest = input[index + 3] as usize;
-                input[dest] = input[index + 1] * input[index + 2];
+                let i1 = memory[index + 1] as usize;
+                let i2 = memory[index + 2] as usize;
+                let dest = memory[index + 3] as usize;
+                memory[dest] = memory[i1] * memory[i2];
             }
             99 => break,
             _ => println!(
                 "something went terribly wrong (opcode {} at {})",
-                input[index], index
+                memory[index], index
             ),
         }
 
         index += 4;
     }
 
-    input[0]
+    memory[0]
 }
